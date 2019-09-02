50ae03694cd257c38e53ff1bf4d0683c44d87279
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2019-09-02 18:02:58 +0200

    Remove not necessary funtion

--- src/utils.rs
+++ src/utils.rs
@@ -17,25 +17,7 @@ pub trait MapReduce {
     fn merge(args: &Self::SplitArgs, subtasks_result: &TaskResult<Self::ExecuteInput, Self::ExecuteOutput>);
 }
 
-
-
 pub fn save_params<SplitOutputType : TaskInput>(output_dir: &Path, split_params: &Vec<SplitOutputType>) -> Result<(), Error> {
-
-    let json_params: Vec<serde_json::Value> = split_params.iter().map(TaskInput::pack_task).collect();
-
-    fs::create_dir_all(output_dir)?;
-
-    let mut subtask_num: u32 = 0;
-    for subtask_params in json_params {
-        let output_file = output_dir.join(format!("params-{}.json", subtask_num));
-        fs::write(output_file, serde_json::to_string_pretty(&subtask_params)?)?;
-
-        subtask_num += 1;
-    }
-    Ok(())
-}
-
-pub fn save_params2<SplitOutputType : TaskInput>(output_dir: &Path, split_params: &Vec<SplitOutputType>) -> Result<(), Error> {
     let json_params: Vec<serde_json::Value> = split_params.iter().map(TaskInput::pack_task).collect();
 
     fs::create_dir_all(output_dir)?;
@@ -51,7 +33,7 @@ pub fn split_step<MapReduceType: MapReduce>() -> Vec<MapReduceType::ExecuteInput
 
     // Split step.
     let split_params = MapReduceType::split(&opt);
-    save_params2(Path::new("results/split/"), &split_params).unwrap();
+    save_params(Path::new("results/split/"), &split_params).unwrap();
 
     return split_params;
 }
