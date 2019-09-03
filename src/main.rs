96febb038cbd4194a6bca65dee3f798109cb709f
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2019-09-03 15:45:47 +0200

    Refactor main as it should be in future

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,15 @@ pub mod utils;
 pub mod png_utils;
 pub mod mandelbrot;
 
-use structopt::*;
-
-use gwasm_api::{Blob, TaskResult, TaskInput};
-
 use crate::utils::MapReduce;
-use crate::mandelbrot::{MandelbrotParams, Mandelbrot, ExecuteParams};
+use crate::mandelbrot::{Mandelbrot};
 
 
 
 fn main() {
 
-    //let split_params = utils::split_step::<Mandelbrot>();
+    // Run MapReduce task defined in Mandelbrot struct.
+    // This function will parse command line arguments and dispatch it
+    // to one of split, execute and merge functions.
     utils::dispatch_and_run_command::<Mandelbrot>();
-
-    // Execute step for all subtasks.
-
-    // Temporary
-//    let opt = MandelbrotParams::from_args();
-//
-//    let mut results = Vec::new();
-//    for subtask_params in split_params.into_iter() {
-//        results.push((subtask_params.clone(), Mandelbrot::execute(subtask_params)));
-//    }
-//
-//    // Merge step.
-//    Mandelbrot::merge(&opt, &results);
 }
