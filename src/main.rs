d450ee6aaa31e390515f1d2050d5eb8ba6d7c086
Jakub Konka <jakub.konka@golem.network>
2019-09-23 10:51:34 +0200

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-pub mod utils;
-pub mod png_utils;
 pub mod mandelbrot;
+pub mod png_utils;
+pub mod utils;
 
+use crate::mandelbrot::Mandelbrot;
 use gwasm_api::dispatcher;
-use crate::mandelbrot::{Mandelbrot};
-
-
 
 fn main() {
-
     // This function will parse command line arguments and dispatch it
     // to one of split, execute and merge functions.
     dispatcher::run(&Mandelbrot::split, &Mandelbrot::execute, &Mandelbrot::merge).unwrap();
--- src/png_utils.rs
+++ src/png_utils.rs
@@ -1,24 +1,25 @@
 use failure::{Error, Fail};
-use std::io::{Write, Read};
-
+use std::io::{Read, Write};
 
 #[derive(Debug, Fail)]
 pub enum SaveFileError {
-    #[fail(display = "Can't save file. Buffer size doesn't match expected width {} and height {}.", width, height)]
-    NotMatchingSize {
-        width: u32,
-        height: u32,
-    },
+    #[fail(
+        display = "Can't save file. Buffer size doesn't match expected width {} and height {}.",
+        width, height
+    )]
+    NotMatchingSize { width: u32, height: u32 },
     #[fail(display = "Can't find parent")]
     NoParent,
 }
 
-
-
-pub fn save_file(output: &mut dyn Write, data: &Vec<u8>, width: u32, height: u32) -> Result<(), Error> {
-
+pub fn save_file(
+    output: &mut dyn Write,
+    data: &Vec<u8>,
+    width: u32,
+    height: u32,
+) -> Result<(), Error> {
     if data.len() != (width * height) as usize {
-        return Err(SaveFileError::NotMatchingSize{ width, height })?;
+        return Err(SaveFileError::NotMatchingSize { width, height })?;
     }
 
     let mut encoder = png::Encoder::new(output, width, height);
@@ -37,8 +38,5 @@ pub fn load_file(output: &mut dyn Read) -> Vec<u8> {
 
     reader.next_frame(&mut buf).unwrap();
 
-    return buf
+    return buf;
 }
-
-
-
